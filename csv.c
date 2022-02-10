4053dabf72b1d8b694868e6e36955fe15b681039
wenhuihuang <wenhuihu@buffalo.edu>
2022-02-10 16:41:04 -0500

    complete structure

--- csv.c
+++ csv.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+   	
+    
+    return 0;
+}
+
+// -f
+int num_columns() {
 	return 0;
 }
+
+// -r
+int num_rows() {
+	return 0;
+}
+
+// -h
+int check_header() {
+	return 0;
+}
+
+// max
+int max_data() {
+	return 0;
+}
+
+// min
+int min_data() {
+	return 0;
+}
+
+// mean
+int mean_data() {
+	return 0;
+}
+
+// records
+int find_data() {
+
+}
+
+
 
